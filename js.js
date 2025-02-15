55aed061fd74d7b488e9e4c3986ccc8bf7314f4a
cjlindahl <carsonlindahl@gmail.com>
2025-02-15 11:09:59 -0700

    Create js.js

---
+++ js.js
@@ -0,0 +1,5 @@
+// Scroll Reveal Animations
+window.sr = ScrollReveal({ reset: true });
+sr.reveal('.feature-item', { delay: 200, duration: 1000, distance: '50px', origin: 'bottom' });
+sr.reveal('.spec-item', { delay: 200, duration: 1000, distance: '50px', origin: 'bottom' });
+sr.reveal('.design-gallery img', { delay: 200, duration: 1000, distance: '50px', origin: 'bottom' });
